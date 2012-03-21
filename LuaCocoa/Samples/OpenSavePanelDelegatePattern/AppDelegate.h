e4e069e17aa43ffbd7910189cd969df5d85a88b0
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-03-21 16:58:04 +0900

    Initial commit

---
+++ LuaCocoa/Samples/OpenSavePanelDelegatePattern/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  OpenSavePanelDelegatePattern
+//
+//  Created by Eric Wing on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+@class LuaCocoa;
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+		LuaCocoa* luaCocoa;
+}
+@property (assign) IBOutlet NSWindow *window;
+
+@end
